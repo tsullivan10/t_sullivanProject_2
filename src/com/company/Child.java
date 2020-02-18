035a08f9f392715b31688e9b3db223a1dc5ae423
tsullivan10 <60237776+tsullivan10@users.noreply.github.com>
2020-02-18 17:03:12 -0500

    recent update at 5:00

--- src/com/company/Child.java
+++ src/com/company/Child.java
@@ -4,22 +4,25 @@ public class Child {
     private int age;
     private boolean siblingInProgram;
     private int yearsOfAttendance;
-    private String name;
+    private String Name;
     private int daycareID;
     private static int nextID = 1000;
 
-    public Child(){
-        name = new name();
+    public Child(String name, boolean siblingPlan, int currentAge){
+        Name = new name();
         siblingInProgram = new siblingPlan();
         age = new currentAge();
+        yearsOfAttendance = 0;
+        daycareID = nextID;
+        nextID++;
     }
 
     public double calculateBill(){
 
     }
 
-    public Child(name String, siblingPlan boolean, currentAge int, ){
 
-    }
+
+
 
 }
